c1512988af23326827f3104a52d5e09b105017bd
sethu-7 <sethu7praveen@gmail.com>
2023-04-21 09:31:26 +0530

    try

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express=require('express');
 const sqlite3=require('sqlite3').verbose();
 const app=express();
+const multer=require('multer')
 const path=require('path')
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 app.set('views','views')
+
 
 
 app.listen(3300);
