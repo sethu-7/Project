164af126c4cd4e4867aa8bb6f9dc092e9f290d90
poorvitha7 <129936261+poorvitha7@users.noreply.github.com>
2023-04-23 22:47:36 +0530

    changes

--- index.js
+++ index.js
@@ -50,135 +50,174 @@ db.once('open', function () {
 // Render offers in EJS file
 
 const offerSchema = new mongoose.Schema({
-    image:String,
-    title:String,
-    description:String
-     
-   });
-   
-   const Offer=mongoose.model("Offer",offerSchema);
+    image: String,
+    title: String,
+    description: String
+
+});
+
+const Offer = mongoose.model("Offer", offerSchema);
 
 app.get('/offers', async (req, res) => {
     try {
-      const offers = await Offer.find();
-      res.render('offers', { offers });
+        const offers = await Offer.find();
+        res.render('offers', { offers });
     } catch (err) {
-      console.error('Error retrieving offers:', err);
-      res.status(500).send('Internal server error');
+        console.error('Error retrieving offers:', err);
+        res.status(500).send('Internal server error');
     }
-  });
+});
 
 
-  // Handle form submission
-  app.post('/offers', async (req, res) => {
+// Handle form submission
+app.post('/offers', async (req, res) => {
     try {
-      const { title, description, image } = req.body;
-  
-      // Create a new offer object and set its properties
-      const offer = new Offer({
-        title,
-        description,
-         image // set the image field to the imageUrl from the req.body
-      });
-  
-      // Save the offer object to the database
-      await offer.save();
-  
-      res.status(201).json({ message: 'Offer added successfully' });
+        const { title, description, image } = req.body;
+
+        // Create a new offer object and set its properties
+        const offer = new Offer({
+            title,
+            description,
+            image // set the image field to the imageUrl from the req.body
+        });
+
+        // Save the offer object to the database
+        await offer.save();
+
+        res.status(201).json({ message: 'Offer added successfully' });
     } catch (err) {
-      console.error('Error adding offer:', err);
-      res.status(500).json({ error: 'Internal server error' });
+        console.error('Error adding offer:', err);
+        res.status(500).json({ error: 'Internal server error' });
     }
-  });
+});
 
 
 
 
-  //deletion
+//deletion
 // Handle DELETE request to delete an offer by its title
 app.delete('/offers/:title', async (req, res) => {
     try {
-      const title = req.params.title;
-  
-      // Find the offer with the matching title and delete it
-      const deletedOffer = await Offer.findOneAndDelete({ title });
-  
-      if (!deletedOffer) {
-        // If no offer with the matching title was found, return a 404 Not Found error
-        res.status(404).send('Offer not found');
-        return;
-      }
-  
-      res.status(204).send();
-    } catch (err) {
-      console.error('Error deleting offer:', err);
-      res.status(500).send('Internal server error');
-    }
-  });
-  
+        const title = req.params.title;
 
-  app.get('/medicines', async (req, res) => {
+        // Find the offer with the matching title and delete it
+        const deletedOffer = await Offer.findOneAndDelete({ title });
+
+        if (!deletedOffer) {
+            // If no offer with the matching title was found, return a 404 Not Found error
+            res.status(404).send('Offer not found');
+            return;
+        }
+
+        res.status(204).send();
+    } catch (err) {
+        console.error('Error deleting offer:', err);
+        res.status(500).send('Internal server error');
+    }
+});
+
+
+app.get('/medicines', async (req, res) => {
     try {
-      const medicines = await medicines.find();
-      res.render('medicines', { medicines});
+        const medicines = await medicines.find();
+        res.render('medicines', { medicines });
     } catch (err) {
-      console.error('Error retrieving medicines:', err);
-      res.status(500).send('Internal server error');
+        console.error('Error retrieving medicines:', err);
+        res.status(500).send('Internal server error');
     }
-  });
+});
 
 
-  // Handle form submission
-  app.post('/medicines', async (req, res) => {
+// Handle form submission
+app.post('/medicines', async (req, res) => {
     try {
-      const { name, m_id,cost,description, added  } = req.body;
-  
-      // Create a new offer object and set its properties
-      const med = new medicines({
-       name,
-       m_id,
-       cost,
-        description,
-        added
-        // set the image field to the imageUrl from the req.body
-      });
-  
-      // Save the offer object to the database
-      await med.save();
-  
-      res.status(201).json({ message: 'Medicine added successfully' });
+        const { name, m_id, cost, description, added } = req.body;
+
+        // Create a new offer object and set its properties
+        const med = new medicines({
+            name,
+            m_id,
+            cost,
+            description,
+            added
+            // set the image field to the imageUrl from the req.body
+        });
+
+        // Save the offer object to the database
+        await med.save();
+
+        res.status(201).json({ message: 'Medicine added successfully' });
     } catch (err) {
-      console.error('Error adding medicine:', err);
-      res.status(500).json({ error: 'Internal server error' });
+        console.error('Error adding medicine:', err);
+        res.status(500).json({ error: 'Internal server error' });
     }
-  });
+});
 
 
 
 
-  //deletion
+//deletion
 // Handle DELETE request to delete an offer by its title
 app.delete('/medicines/:name', async (req, res) => {
     try {
-      const name = req.params.name;
-  
-      // Find the offer with the matching title and delete it
-      const deleted_med = await medicines.findOneAndDelete({ name });
-  
-      if (!deleted_med) {
-       
-        res.status(404).send('medicine not found');
-        return;
-      }
-  
-      res.status(204).send();
-    } catch (err) {
-      console.error('Error deleting medicine:', err);
-      res.status(500).send('Internal server medicine');
-    }
-  });
-  
+        const name = req.params.name;
 
+        // Find the offer with the matching title and delete it
+        const deleted_med = await medicines.findOneAndDelete({ name });
+
+        if (!deleted_med) {
+
+            res.status(404).send('medicine not found');
+            return;
+        }
+
+        res.status(204).send();
+    } catch (err) {
+        console.error('Error deleting medicine:', err);
+        res.status(500).send('Internal server medicine');
+    }
+});
+
+
+
+router.post("/viewcart_final", async (req, res) => {
+    const {  name,m_id, cost } = req.body;
+
+    const userId = "5de7ffa74fff640a0491bc4f"; //TODO: the logged in user id
+
+    try {
+        let cart = await medicines.findOne({ userId });
+
+        if (cart) {
+            //cart exists for user
+
+            let itemIndex = cart.findIndex(p => p.m_id == m_id);
+
+            if (itemIndex > -1) {
+                //product exists in the cart, update the quantity
+                let productItem = cart.[itemIndex];
+                
+                cart.products[itemIndex] = productItem;
+            } else {
+                //product does not exists in cart, add new item
+                cart.products.push({ name, m_id, cost });
+            }
+            cart = await cart.save();
+            return res.status(201).send(cart);
+        } else {
+            //no cart for user, create new cart
+            const newCart = await medicines.create({
+                userId,
+                products: [{ name, m_id, cost}]
+            });
+
+            return res.status(201).send(newCart);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong");
+    }
+});
 
 
 app.get('/doctor_project_final', (req, res) => {
@@ -250,9 +289,9 @@ app.post('/login', async (req, res, next) => {
             password: req.body.password
         })
 
-       
 
-        
+
+
         if (user) {
             req.session.email = user.email;
 
